5cc8de77cfb5f68461d4b9ca27e85ef662cc41aa
pratikupadhyay8504-design <pratik.upadhyay8504@gmail.com>
2026-09-15 14:04:30 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,17 +3,17 @@ public:
     bool isAnagram(string s, string t) {
         vector<int>freq1(256,0);
         vector<int>freq2(256,0);
-        int i;
-        for(i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             freq1[s[i]]++;
         }
-        for(i=0;i<t.length();i++){
-             freq2[t[i]]++;
-           
-        }  
-        if(freq1==freq2) return true;
-        else{
+         for(int i=0;i<t.length();i++){
+            freq2[t[i]]++;
+         }
+         if(freq1==freq2) return true;
+         else{
             return false;
-        }      
+         }
+
+       
     }
 };
