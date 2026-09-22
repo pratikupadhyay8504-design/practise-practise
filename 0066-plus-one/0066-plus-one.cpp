1929bd5d9fb620fea8f2be6b15f0a11b0f461c58
pratikupadhyay8504-design <pratik.upadhyay8504@gmail.com>
2026-09-22 14:00:51 +0530

    Time: 0 ms (100%), Space: 11.8 MB (25.77%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& nums) {
+        int i,n=nums.size();
+        for(i=n-1;i>=0;i--){
+            if(nums[i]<9){
+                nums[i]++;
+                return nums;
+            }
+            nums[i]=0;
+
+            }
+            nums.insert(nums.begin(),1);
+            return nums;
+        }
+        
+    
+};
